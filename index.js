3b65a698c1aac1c03e14d5f82ee8c87583e21d23
Marco Filho <markimkr@gmail.com>
2022-03-22 15:26:50 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let contador = 0;
+
+const url = "dado de 1gb";
+
+function inicio() {
+  contador++;
+  console.log(`Iniciou o programa pela ${contador}ª vez`);
+  const nomes = ["xablau", "marco", "jonathan"];
+
+  nomes.forEach((nome) => console.log(nome));
+
+  if (true) {
+    setTimeout(() => {
+      console.log("Passou quatro segundos...");
+      inicio();
+    }, 4000);
+  }
+}
+
+console.log(url);
+
+inicio();
